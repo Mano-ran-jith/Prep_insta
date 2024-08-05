c8aa03869bbf1dd2e507431d7222110269d83fc1
Manoranjith <138356867+Mano-ran-jith@users.noreply.github.com>
2024-08-05 16:02:29 +0530

    Add files via upload

---
+++ Day14.py
@@ -0,0 +1,9 @@
+num1 = 371
+num3 = 0
+for i in str(num1):
+    num2 = len(str(num1))
+    num3 += (int(i) ** num2)
+if num1 == num3:
+    print("Amstrong Number")
+else:
+    print("Not an Amstrong")

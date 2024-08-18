512d19f4767255c40e8e58c41301ea79cdee3c5c
Manoranjith <138356867+Mano-ran-jith@users.noreply.github.com>
2024-08-18 20:47:17 +0530

    Add files via upload

---
+++ Day16.py
@@ -0,0 +1,11 @@
+# fibonacci series upto n terms
+
+num = 10
+n1, n2 = 0, 1
+print("Fibonacci Series:", n1, n2, end=" ")
+for i in range(2, num):
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
+    print(n3, end=" ")
+print()
